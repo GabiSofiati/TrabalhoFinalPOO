6c1b200af254121b7b3903ac2dea432dda339ce0
ppaulof <paulofontenele@furb.br>
2024-11-27 21:01:13 -0300

    Primeiras classes prontas

---
+++ src/Trabalho/model/Lancamento.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Trabalho.model;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+
+/**
+ * Representa um lancamento
+ *
+ * @author Paulo Fontenele da Silva
+ */
+public class Lancamento {
+
+    private double valor;
+    private LocalDate data;
+
+    public Lancamento(double valor, LocalDate data) {
+        this.valor = valor;
+        this.data = data;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    
+    
+}
---
+++ src/Trabalho/model/TipoCategoria.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Trabalho.model;
+
+/**
+ * Enum que representa o tipo de uma categoria.
+ * Identifica a categoria como sendo de receita ou despesa.
+ *
+ * @author Paulo Fontenele da Silva
+ */
+public enum TipoCategoria {
+    
+    RECEITA,
+    DESPESA;
+    
+}
